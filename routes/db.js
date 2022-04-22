ace5c289db42c8af89b7efbfd17f6c9b324b64a9
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-04-22 10:15:50 -0400

    Got routes to work, adding content now works

--- routes/db.js
+++ routes/db.js
@@ -1,4 +1,5 @@
 const db = require("express").Router();
+const { v4: uuidv4 } = require('uuid');
 
 const {
   readFromFile,
@@ -10,6 +11,55 @@ const {
 db.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+//GetRoute for retreiving specific note
+db.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID')
+    });
+});
+
+//Delete a specific note
+db.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      //creates a new array of all notes except for the one with a matching ID
+      const result = json.filter((note) => note.note_id !== noteId);
+      //saves the array to the db json
+      writeToFile('./db/db.json', result);
+      //Respond to the delete request
+
+      res.json(`Item ${noteId} jas been deleted`);
+    });
+});
+
+//POST route for new Note
+db.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4()
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error(`Error in adding note`);
+  }
+})
 
 
 module.exports = db;
